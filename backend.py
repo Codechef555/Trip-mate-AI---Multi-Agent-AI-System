e76cfa07ea083dba6e6844693f977f30137dd46e
Codechef555 <mdkaraamathullahsheriff@gmail.com>
2026-08-07 04:14:06 +0530

    Added function for FastAPI

--- backend.py
+++ backend.py
@@ -178,4 +178,38 @@ checkpointer.setup()
 
 travel_graph = graph.compile(checkpointer=checkpointer)
 
-#
+#FastAPI function
+def run_travel_agent(user_input: str, thread_id: str | None = None):
+    if not thread_id:
+        thread_id = f"user_{uuid.uuid4().hex}"
+
+    config = {
+        "configurable": {
+            "thread_id": thread_id
+        }
+    }
+
+    result = travel_graph.invoke(
+        {
+            "messages": [
+                HumanMessage(content=user_input)
+            ],
+            "user_query": user_input,
+            "flight_results": "",
+            "hotel_results": "",
+            "itinerary": "",
+            "llm_calls": 0
+        },
+        config=config
+    )
+
+    final_answer = result["messages"][-1].content
+
+    return {
+        "thread_id": thread_id,
+        "answer": final_answer,
+        "flight_results": result.get("flight_results", ""),
+        "hotel_results": result.get("hotel_results", ""),
+        "itinerary": result.get("itinerary", ""),
+        "llm_calls": result.get("llm_calls", 0),
+    }
