90adfa8191c40e71c150537620f85d95f4b2c20f
Codechef555 <mdkaraamathullahsheriff@gmail.com>
2026-08-07 03:30:25 +0530

    Import packages for backend file

--- backend.py
+++ backend.py
@@ -1,1 +1,22 @@
-
+import os 
+import certifi
+from dotenv import load_dotenv
+from typing import TypedDict, Annotated
+import operator
+import uuid
+import psycopg
+from psycopg.rows import dict_row
+from langgraph.graph import StateGraph, START, END
+from langgraph.checkpoint.postgres import PostgresSaver
+from langchain_core.messages import (
+    AnyMessage,
+    HumanMessage,
+    AIMessage,
+    SystemMessage,
+)
+from langchain_groq import ChatGroq
+from tools.tavily_tool import tavily_search
+from tools.flight_tool import search_flights
+load_dotenv()
+os.environ["SSL_CERT_FILE"] = certifi.where()
+os.environ["REQUESTS_CA_BUNDLE"] = certifi.where()
