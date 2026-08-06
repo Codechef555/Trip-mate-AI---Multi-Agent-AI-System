a5d547412263d9b3f17a4229b076cc0b5c6d196d
Codechef555 <mdkaraamathullahsheriff@gmail.com>
2026-08-07 04:10:39 +0530

    Added Graph

--- backend.py
+++ backend.py
@@ -149,3 +149,17 @@ Important:
         "messages": [response],
         "llm_calls": state.get("llm_calls", 0) + 1
     }
+
+#Graph setup
+graph = StateGraph(TravelState)
+
+graph.add_node("flight_agent", flight_agent)
+graph.add_node("hotel_agent", hotel_agent)
+graph.add_node("itinerary_agent", itinerary_agent)
+graph.add_node("final_agent", final_agent)
+
+graph.add_edge(START, "flight_agent")
+graph.add_edge("flight_agent", "hotel_agent")
+graph.add_edge("hotel_agent", "itinerary_agent")
+graph.add_edge("itinerary_agent", "final_agent")
+graph.add_edge("final_agent", END)
