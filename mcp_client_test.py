1bbd83c91711c4b4645ca5962bfd0baa57d9cd43
Codechef555 <mdkaraamathullahsheriff@gmail.com>
2026-08-11 02:47:36 +0530

    configure SSL certificates and load environment variables in mcp_client_test.py

--- mcp_client_test.py
+++ mcp_client_test.py
@@ -3,3 +3,9 @@ import asyncio
 import certifi 
 from dotenv import load_dotenv
 from langchain_mcp_adapters.client import MultiServerMCPClient
+
+os.environ["SSL_CERT_FILE"] = certifi.where()
+os.environ["REQUESTS_CA_BUNDLE"] = certifi.where()
+
+load_dotenv()
+TAVILY_API_KEY = os.getenv("TAVILY_API_KEY")
