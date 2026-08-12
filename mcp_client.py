9366e79b2c11c4ab55ff70ede5ed9f7c299c2ed5
Codechef555 <mdkaraamathullahsheriff@gmail.com>
2026-08-13 03:31:02 +0530

    load external API keys from environment variables in mcp_client

--- mcp_client.py
+++ mcp_client.py
@@ -11,3 +11,8 @@ os.environ["SSL_CERT_FILE"] = certifi.where()
 os.environ["REQUESTS_CA_BUNDLE"] = certifi.where()
 
 load_dotenv()
+
+TAVILY_API_KEY = os.getenv("TAVILY_API_KEY")
+AVIATION_STACK_API_KEY = os.getenv("AVIATIONSTACK_API_KEY")
+OPENWEATHER_API_KEY = os.getenv("OPENWEATHER_API_KEY")
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
