7b322dfcb7376a3e0abbf693b78cde03fa353eb0
Codechef555 <mdkaraamathullahsheriff@gmail.com>
2026-08-13 03:30:21 +0530

    configure SSL certificate environment variables and load .env in mcp_client

--- mcp_client.py
+++ mcp_client.py
@@ -6,3 +6,8 @@ import certifi
 from dotenv import load_dotenv
 from langchain_mcp_adapters.client import MultiServerMCPClient
 from langchain_groq import ChatGroq
+
+os.environ["SSL_CERT_FILE"] = certifi.where()
+os.environ["REQUESTS_CA_BUNDLE"] = certifi.where()
+
+load_dotenv()
