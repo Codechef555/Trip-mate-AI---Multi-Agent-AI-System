6b16f9676f5d6d895dcc8f942215bd3bdb24e574
Md.Karaamathullah Sheriff <90059832+Codechef555@users.noreply.github.com>
2026-08-14 04:16:15 +0530

    get current weather function

--- custom_weather_mcp_server.py
+++ custom_weather_mcp_server.py
@@ -10,3 +10,27 @@ mcp = FastMCP("Weather MCP Server")
 
 OPENWEATHER_API_KEY = os.getenv("OPENWEATHER_API_KEY")
 
+def get_current_weather(city: str):
+
+    response = requests.get(
+        "https://api.openweathermap.org/data/2.5/weather",
+        params={
+            "q": city,
+            "appid": OPENWEATHER_API_KEY,
+            "units": "metric"
+        }
+    )
+
+    data = response.json()
+
+    if response.status_code != 200:
+        return data
+
+    return {
+        "city": data["name"],
+        "temperature_c": data["main"]["temp"],
+        "feels_like_c": data["main"]["feels_like"],
+        "humidity": data["main"]["humidity"],
+        "condition": data["weather"][0]["description"],
+        "wind_speed": data["wind"]["speed"]
+    }
