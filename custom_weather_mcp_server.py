6e6a212b89b3f43a5f1ac6dc3fb798cb883aefc8
Md.Karaamathullah Sheriff <90059832+Codechef555@users.noreply.github.com>
2026-08-14 04:15:13 +0530

    import the packages and api key for weather mcp

--- custom_weather_mcp_server.py
+++ custom_weather_mcp_server.py
@@ -1,2 +1,12 @@
 from mcp.server.fastmcp import FastMCP
- 
+import requests
+import os 
+from dotenv import load_dotenv
+
+load_dotenv()
+
+mcp = FastMCP("Weather MCP Server")
+
+
+OPENWEATHER_API_KEY = os.getenv("OPENWEATHER_API_KEY")
+
