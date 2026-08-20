a3a1e95d2159be914ef7d52475ef02801aabceab
Md.Karaamathullah Sheriff <90059832+Codechef555@users.noreply.github.com>
2026-08-20 13:10:48 +0530

    Comment the previous edges of the graph

--- backend.py
+++ backend.py
@@ -618,12 +618,12 @@ graph.add_node("itinerary_agent", itinerary_agent)
 graph.add_node("final_agent", final_agent)
 
 #Adds the node structure from start to end 
-graph.add_edge(START, "flight_agent")
-graph.add_edge("flight_agent", "hotel_agent")
-graph.add_edge("hotel_agent", "weather_agent")
-graph.add_edge('weather_agent','itinerary_agent')
-graph.add_edge("itinerary_agent", "final_agent")
-graph.add_edge("final_agent", END)
+#graph.add_edge(START, "flight_agent")
+#graph.add_edge("flight_agent", "hotel_agent")
+#graph.add_edge("hotel_agent", "weather_agent")
+#graph.add_edge('weather_agent','itinerary_agent')
+#graph.add_edge("itinerary_agent", "final_agent")
+#graph.add_edge("final_agent", END)
 
 #PostgreSQL Checkpointer
 DATABASE_URL = get_database_url()
